c67ee08cee6652a6d4248d7c701d93079335dc75
emka <mitja@kleider.name>
2009-09-28 12:55:51 +0200

    improved translation strings

--- client/translation.js
+++ client/translation.js
@@ -21,9 +21,14 @@ function set_language (lang) {
 }
 
 function translate_sidebar () {
-    document.getElementById('introduction').innerHTML = '<p>'+_('Feel free to put the modifications you would like to see on ')+'<a href="http://www.openstreetmap.org">OpenStreetMap</a>'+_(' on the map.')+'</p><p><span class="warn">'+_('Do NOT use licensed data')+'</span>'+_(' like paper maps, Google Maps, etc. Use only your knowledge of the reality or public domain data.')+'</p><p>'+_('According to the OpenStreetMap license, the data that you add on the map will be licensed ')+'<a href="http://creativecommons.org/licenses/by-sa/3.0/">CC BY-SA</a>.</p><p>'+_('To add a bug, click at the desired spot in the map.')+'</p><p><a href="more.html">'+_('More information')+'</a></p>';
+    var introduction = _('Feel free to put the modifications you would like to see on {{OpenStreetMap}} on the map.');
+    introduction = introduction.replace('{{OpenStreetMap}}','<a href="http://www.openstreetmap.org">OpenStreetMap</a>');
+    var license = _('According to the OpenStreetMap license, the data that you add on the map will be licensed {{License}}.');
+    license = license.replace('{{License}}','<a href="http://creativecommons.org/licenses/by-sa/3.0/">CC BY-SA</a>');
+    document.getElementById('introduction').innerHTML = '<p>'+introduction+'</p><p><span class="warn">'+_('Do NOT use licensed data')+'</span>'+_(' like paper maps, Google Maps, etc. Use only your knowledge of the reality or public domain data.')+'</p><p>'+license+'</p><p>'+_('To add a bug, click at the desired spot in the map.')+'</p><p><a href="more.html">'+_('More information')+'</a></p>';
     document.getElementById('linkbox-header').innerHTML = _('current view');
 
+    document.title = 'OpenStreetBugs - '+_('Help us to improve OpenStreetMap');
 }
 
 var translation = {};
